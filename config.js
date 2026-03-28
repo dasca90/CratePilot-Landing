8e7d8db37c9ecae19e8e89f0311c1785ee69611c
dasca90 <alexdasca@gmail.com>
2026-03-28 19:58:14 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,4 +2,4 @@
 // Update this to your real GitHub Release asset URL.
 // Example:
 // https://github.com/<owner>/<repo>/releases/download/v2.1.0/Crate-Pilot-Setup-2.1.0.exe
-window.CRATE_PILOT_DOWNLOAD_URL = "https://github.com/OWNER/REPO/releases/latest/download/Crate-Pilot-Setup.exe";
+window.CRATE_PILOT_DOWNLOAD_URL = "https://github.com/dasca90/CratePilot-Landing/releases/download/2.1.0-Beta/CratePilot-Trial-Beta-2.1.0-beta.1.exe";
